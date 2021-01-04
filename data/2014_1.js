b70f07e0375c3bcd4fb7ab6631a36c51d3d57d3c
Fangelito <76205983+Fangelito@users.noreply.github.com>
2021-01-04 10:02:04 +0100

    Guadiamar

---
+++ data/2014_1.js
@@ -0,0 +1,12 @@
+var json_2014_1 = {
+"type": "FeatureCollection",
+"name": "2014_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "AS_SOIL_PS": 59.54329, "CD_SOIL_PS": 1.55874, "CU_SOIL_PS": 88.7186, "PB_SOIL_PS": 134.18637, "ZN_SOIL_PS": 446.58174, "CD_SOIL_SO": 0.66258, "CU_SOIL_SO": 23.10869, "PB_SOIL_SO": 16.54114, "ZN_SOIL_SO": 67.26939, "TREAT": "COMPOST (20) +SUGAR BEET LIME 1(20)" }, "geometry": { "type": "Point", "coordinates": [ -6.25102433080133, 37.315587726782354 ] } },
+{ "type": "Feature", "properties": { "AS_SOIL_PS": 53.95383, "CD_SOIL_PS": 1.62789, "CU_SOIL_PS": 82.26999, "PB_SOIL_PS": 117.31619, "ZN_SOIL_PS": 416.71592, "CD_SOIL_SO": 0.76618, "CU_SOIL_SO": 21.34397, "PB_SOIL_SO": 17.27522, "ZN_SOIL_SO": 69.29626, "TREAT": "COMPOST (20) +SUGAR BEET LIME 1(20)" }, "geometry": { "type": "Point", "coordinates": [ -6.259403188159995, 37.311608847753618 ] } },
+{ "type": "Feature", "properties": { "AS_SOIL_PS": 50.26506, "CD_SOIL_PS": 3.2738, "CU_SOIL_PS": 142.36991, "PB_SOIL_PS": 110.28812, "ZN_SOIL_PS": 768.68809, "CD_SOIL_SO": 1.74186, "CU_SOIL_SO": 45.34689, "PB_SOIL_SO": 25.55524, "ZN_SOIL_SO": 147.5099, "TREAT": "SUGAR BEET LIME 1 (20)" }, "geometry": { "type": "Point", "coordinates": [ -6.264360462002601, 37.291920476525256 ] } },
+{ "type": "Feature", "properties": { "AS_SOIL_PS": 77.53639, "CD_SOIL_PS": 2.28365, "CU_SOIL_PS": 130.431, "PB_SOIL_PS": 182.0276, "ZN_SOIL_PS": 729.67875, "CD_SOIL_SO": 1.069, "CU_SOIL_SO": 35.76103, "PB_SOIL_SO": 33.00699, "ZN_SOIL_SO": 100.71468, "TREAT": "NO TREAT" }, "geometry": { "type": "Point", "coordinates": [ -6.258762341934448, 37.291552329775605 ] } },
+{ "type": "Feature", "properties": { "AS_SOIL_PS": 45.63903, "CD_SOIL_PS": 2.6006, "CU_SOIL_PS": 143.58699, "PB_SOIL_PS": 110.42457, "ZN_SOIL_PS": 655.36182, "CD_SOIL_SO": 1.21119, "CU_SOIL_SO": 38.39288, "PB_SOIL_SO": 21.84592, "ZN_SOIL_SO": 87.02457, "TREAT": "NO TREAT" }, "geometry": { "type": "Point", "coordinates": [ -6.2671356405699, 37.265624833436611 ] } }
+]
+}
